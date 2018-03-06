86f2bf7790c41ad7b2ff67a243b910193c9dbb74
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2018-03-06 12:51:22 -0300

    Customizing first page

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
-  
+  <div>
+    <h1>{{title}}</h1>
+    <img src="http://tudosobrecachorros.com.br/como-criar-um-cachorro-independente-e-porque/" alt="" />
+  </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      title: 'Alurapic'
+    }
+  }
 }
 </script>
 
