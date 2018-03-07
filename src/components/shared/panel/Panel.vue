baebad018b6345b52e8e2625674f8749a900d07e
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2018-03-07 13:03:58 -0300

    Setting component style as scoped

--- src/components/shared/panel/Panel.vue
+++ src/components/shared/panel/Panel.vue
@@ -12,7 +12,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
   .panel {
     padding: 0 auto;
     border: solid 2px grey;
@@ -31,5 +31,8 @@ export default {
     margin: 0 0 15px 0;
     padding: 10px;
     text-transform: uppercase;
+  }
+  * {
+    box-shadow: 5px 5px 5px;
   }
 </style>
