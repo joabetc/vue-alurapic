a3be02e3b774af976ad6176985bd84e3a1bc471e
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2018-03-07 12:55:41 -0300

    Creating a new component

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,21 @@
     <h1 class="centered">{{ title }}</h1>
     <ul class="photo-list">
       <li class="photo-list-item" v-for="photo of photos">
-        <div class="panel">
-          <h2 class="panel-title">{{ photo.titulo }}</h2>
-          <div class="panel-content">
+        <my-panel :title="photo.titulo">
             <img class="responsive-image" :src="photo.url" :alt="photo.title" />
-          </div>
-        </div>
+        </my-panel>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import Panel from './components/shared/panel/Panel.vue';
+
 export default {
+  components: {
+    'my-panel': Panel
+  },
   data() {
     return {
       title: 'Alurapic',
@@ -47,25 +49,5 @@ export default {
   }
   .responsive-image {
     width: 100%;
-  }
-  /* panel style */
-  .panel {
-    padding: 0 auto;
-    border: solid 2px grey;
-    display: inline-block;
-    margin: 5px;
-    box-shadow: 5px 5px 10px grey;
-    width: 200px;
-    height: 100%;
-    vertical-align: top;
-    text-align: center;
-  }
-  .panel .panel-titulo {
-    text-align: center;
-    border: solid 2px;
-    background: lightblue;
-    margin: 0 0 15px 0;
-    padding: 10px;
-    text-transform: uppercase;
   }
 </style>
