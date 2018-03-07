f72d0a0cf79dfb105d3e57d3c6dec26b94c97080
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2018-03-07 12:38:26 -0300

    Adding a panel

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,12 @@
     <h1 class="centered">{{ title }}</h1>
     <ul class="photo-list">
       <li class="photo-list-item" v-for="photo of photos">
-        <img :src="photo.url" :alt="photo.title" />
+        <div class="panel">
+          <h2 class="panel-title">{{ photo.titulo }}</h2>
+          <div class="panel-content">
+            <img :src="photo.url" :alt="photo.title" />
+          </div>
+        </div>
       </li>
     </ul>
   </div>
@@ -39,5 +44,25 @@ export default {
   }
   .photo-list .photo-list-item {
     display: inline-block;
+  }
+  /* panel style */
+  .panel {
+    padding: 0 auto;
+    border: solid 2px grey;
+    display: inline-block;
+    margin: 5px;
+    box-shadow: 5px 5px 10px grey;
+    width: 200px;
+    height: 100%;
+    vertical-align: top;
+    text-align: center;
+  }
+  .panel .panel-titulo {
+    text-align: center;
+    border: solid 2px;
+    background: lightblue;
+    margin: 0 0 15px 0;
+    padding: 10px;
+    text-transform: uppercase;
   }
 </style>
