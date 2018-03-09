3c4c0cdfa89262e7da90ee5ee5cc7bde0a173ae4
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2018-03-09 12:45:57 -0300

    Creating a new Registration component

---
+++ src/components/registration/Registration.vue
@@ -0,0 +1,15 @@
+<template>
+  <h2 class="centered">Register</h2>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+  .centered {
+    text-align: center;
+  }
+</style>
