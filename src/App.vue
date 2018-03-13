57d25b268434e6d21aae71d13891c53718401bdb
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2018-03-13 12:20:48 -0300

    Creating Home component

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,11 @@
 <template>
   <div class="page-body">
-    <h1 class="centered">{{ title }}</h1>
-    <input type="search" class="filter" @input="filter = $event.target.value" placeholder="Filter by the title of the photo">
-    <ul class="photo-list">
-      <li class="photo-list-item" v-for="photo of filteredPhotos">
-        <my-panel :title="photo.titulo">
-            <responsive-image :url="photo.url" :title="photo.title"/>
-        </my-panel>
-      </li>
-    </ul>
   </div>
 </template>
 
 <script>
-import Panel from './components/shared/panel/Panel.vue';
-import ResponsiveImage from './components/shared/responsive-image/ResponsiveImage.vue';
-
 export default {
-  components: {
-    'my-panel': Panel,
-    'responsive-image': ResponsiveImage
-  },
-  computed: {
-    filteredPhotos() {
-      if (this.filter) {
-        let exp = new RegExp(this.filter.trim(), 'i');
-        return this.photos.filter(photo => exp.test(photo.titulo));
-      } else {
-        return this.photos;
-      }
-    }
-  },
-  data() {
-    return {
-      title: 'Alurapic',
-      photos: [],
-      filter: ''
-    }
-  },
-  created() {
-    this.$http.get('http://localhost:3000/v1/fotos')
-      .then(res => res.json())
-      .then(photos => this.photos = photos, err => console.log(err));
-  }
+
 }
 </script>
 
@@ -51,18 +14,5 @@ export default {
     font-family: Helvetica, sans-serif;
     width: 96%;
     margin: 0 auto;
-  }
-  .centered {
-    text-align: center;
-  }
-  .photo-list {
-    list-style: none;
-  }
-  .photo-list .photo-list-item {
-    display: inline-block;
-  }
-  .filter {
-    display: block;
-    width: 100%;
   }
 </style>
---
+++ src/components/home/Home.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <h1 class="centered">{{ title }}</h1>
+    <input type="search" class="filter" @input="filter = $event.target.value" placeholder="Filter by the title of the photo">
+    <ul class="photo-list">
+      <li class="photo-list-item" v-for="photo of filteredPhotos">
+        <my-panel :title="photo.titulo">
+            <responsive-image :url="photo.url" :title="photo.title"/>
+        </my-panel>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Panel from '../shared/panel/Panel.vue';
+import ResponsiveImage from '../shared/responsive-image/ResponsiveImage.vue';
+
+export default {
+  components: {
+    'my-panel': Panel,
+    'responsive-image': ResponsiveImage
+  },
+  computed: {
+    filteredPhotos() {
+      if (this.filter) {
+        let exp = new RegExp(this.filter.trim(), 'i');
+        return this.photos.filter(photo => exp.test(photo.titulo));
+      } else {
+        return this.photos;
+      }
+    }
+  },
+  data() {
+    return {
+      title: 'Alurapic',
+      photos: [],
+      filter: ''
+    }
+  },
+  created() {
+    this.$http.get('http://localhost:3000/v1/fotos')
+      .then(res => res.json())
+      .then(photos => this.photos = photos, err => console.log(err));
+  }
+}
+</script>
+
+<style>
+  .centered {
+    text-align: center;
+  }
+  .photo-list {
+    list-style: none;
+  }
+  .photo-list .photo-list-item {
+    display: inline-block;
+  }
+  .filter {
+    display: block;
+    width: 100%;
+  }
+</style>
