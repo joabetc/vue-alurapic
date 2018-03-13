d3fc77cb39527e5d16f711e3e2ec346f6cf885c4
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2018-03-13 12:53:53 -0300

    Animating page transition

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,9 @@
         </li>
       </ul>
     </nav>
-    <router-view></router-view>
+    <transition name="page">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
@@ -30,5 +32,11 @@ export default {
     font-family: Helvetica, sans-serif;
     width: 96%;
     margin: 0 auto;
+  }
+  .page-enter, .page-leave-active {
+    opacity: 0;
+  }
+  .page-enter-active, .page.page-leave-active {
+    transition: opacity 4s;
   }
 </style>
