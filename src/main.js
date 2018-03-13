5127defa002eb69b8bced7288e09b6647c91082f
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2018-03-13 12:31:39 -0300

    Setting up VueRouter with routes

--- src/main.js
+++ src/main.js
@@ -3,11 +3,17 @@ import App from './App.vue'
 
 import VueResource from 'vue-resource'
 import VueRouter from 'vue-router'
+import routes from './routes'
 
 Vue.use(VueResource)
 Vue.use(VueRouter)
 
+const router = new VueRouter({
+  routes: routes
+})
+
 new Vue({
   el: '#app',
+  router: router,
   render: h => h(App)
 })
