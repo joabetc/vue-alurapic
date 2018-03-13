7cfecb09ae5fb63669633c3a5ee26236d5d66be0
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2018-03-13 12:29:48 -0300

    Creating routes

---
+++ src/routes.js
@@ -0,0 +1,7 @@
+import Registration from './components/registration/Registration.vue';
+import Home from './components/home/Home.vue';
+
+export const routes = [
+  { path: '', component: Home },
+  { path: '/registration', component: Registration }
+];
