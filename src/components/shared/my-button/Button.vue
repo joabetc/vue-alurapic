63c170964a73db40d158096267bf42c6b1838b4a
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2018-03-14 12:51:51 -0300

    Creating a new Button component

---
+++ src/components/shared/my-button/Button.vue
@@ -0,0 +1,27 @@
+<template>
+  <button class="button button-danger" :type="type">{{ label }}</button>
+</template>
+
+<script>
+export default {
+  props: ['type', 'label']
+}
+</script>
+
+<style>
+  .button {
+    display: inline-block;
+    padding: 10px;
+    border-radius: 3px;
+    margin: 10px;
+    font-size: 1.2em;
+  }
+  .button-danger {
+    background: firebrick;
+    color: white;
+  }
+  .button-default {
+    background: darkcyan;
+    color: white;
+  }
+</style>
