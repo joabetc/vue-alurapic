abf1da786407c8ca0cb92a9d0d84671339080615
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2018-03-15 12:36:50 -0300

    Rotating image on double click event

--- src/directives/Transform.js
+++ src/directives/Transform.js
@@ -3,7 +3,12 @@ import Vue from 'vue';
 Vue.directive('my-transform', {
 
   bind(el, binding, vnode) {
-    console.log(el);
+    let current = 0;
+
+    el.addEventListener('dblclick', function() {
+      current +=90;
+      el.style.transform = `rotate(${current}deg)`;
+    })
   }
 
 });
