8243b61c4917065209cc7639ac1d8910e35987c4
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2018-03-19 13:44:40 -0300

    Filtering menu items to avoid blank item

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import Registration from './components/registration/Registration.vue';
 import Home from './components/home/Home.vue';
 
 export const routes = [
-  { path: '', name: 'home', component: Home, title: 'Home' },
-  { path: '/registration', name: 'registration', component: Registration, title: 'Registration' },
-  { path: '*', component: Home }
+  { path: '', name: 'home', component: Home, title: 'Home', menu: true },
+  { path: '/registration', name: 'registration', component: Registration, title: 'Registration', menu: true },
+  { path: '*', component: Home, menu: false }
 ];
