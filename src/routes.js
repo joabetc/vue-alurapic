13e3d5c43be311fc2329efd39ce888fc9d26fd95
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2018-03-20 12:42:37 -0300

    Creating a route update

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import Home from './components/home/Home.vue';
 
 export const routes = [
   { path: '', name: 'home', component: Home, title: 'Home', menu: true },
-  { path: '/registration/:id', name: 'registration', component: Registration, title: 'Registration', menu: true },
+  { path: '/registration', name: 'registration', component: Registration, title: 'Registration', menu: true },
+  { path: '/registration/:id', name: 'update', component: Registration, title: 'Registration', menu: false },
   { path: '*', component: Home, menu: false }
 ];
