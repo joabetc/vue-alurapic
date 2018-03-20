174bb90326a5641e17b6ea283ba9c7ff867bf52b
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2018-03-20 12:48:39 -0300

    Creating a service method to find a photo by id

--- src/domain/photo/PhotoService.js
+++ src/domain/photo/PhotoService.js
@@ -19,4 +19,10 @@ export default class PhotoService {
     return this._resource
       .delete({ id });
   }
+
+  find(id) {
+    return this._resource
+      .get({ id })
+      .then(res => res.json());
+  }
 }
