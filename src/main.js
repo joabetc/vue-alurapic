b3794833aa7ba399bc1890813b01c02096bac143
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2018-03-21 18:00:25 -0300

    Using request and response interceptors

--- src/main.js
+++ src/main.js
@@ -19,6 +19,17 @@ import 'bootstrap/dist/js/bootstrap.js'
 
 Vue.use(VueResource)
 Vue.http.options.root = process.env.API_URL ? process.env.API_URL : 'http://localhost:3000';
+
+Vue.http.interceptors.push((req, next) => {
+  req.headers.set('Authorization', 'secure info here')
+    console.log('Dealing with the request')
+
+    next(res => {
+      console.log('Dealing with the response')
+      console.log(res.body)
+    })
+})
+
 Vue.use(VueRouter)
 Vue.use(Veevalidate, {
   locale: 'en',
