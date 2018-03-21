9f4876d97c48ddba62e773418d9e9e88f21a92e1
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2018-03-21 17:57:20 -0300

    Checking API_URL existence

--- src/main.js
+++ src/main.js
@@ -18,7 +18,7 @@ import './assets/js/test.js'
 import 'bootstrap/dist/js/bootstrap.js'
 
 Vue.use(VueResource)
-Vue.http.options.root = 'http://localhost:3000';
+Vue.http.options.root = process.env.API_URL ? process.env.API_URL : 'http://localhost:3000';
 Vue.use(VueRouter)
 Vue.use(Veevalidate, {
   locale: 'en',
